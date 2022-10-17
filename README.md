09dbe53e49310cd2ea9f05a06a6da72ce67de041
Norange01 <67119887+Norange01@users.noreply.github.com>
2022-10-16 23:53:06 -0400

    Fixes and adjusts dcm login window

--- README.md
+++
@@ -1,2 +0,0 @@
-# mechtron-3k04
-Simulating a pacemaker for MechTron 3K04
--- dcm/login.py
+++ dcm/login.py
@@ -4,22 +4,25 @@ background = '#333333'
 
 window = tk.Tk()
 window.title("Login form")
-window.geometry('340x440')
+window.geometry('260x190')
+window.minsize(260, 190)
+window.maxsize(260, 190)
 #window.configure(bg=background)
 
 login_label = tk.Label(window, text="Login")
-username_label = tk.Label(window, text="Username")
+username_label = tk.Label(window, text="       Username ")
 username_entry = tk.Entry(window)
-password_label = tk.Label(window, text="Password")
+password_label = tk.Label(window, text="       Password ")
 password_entry = tk.Entry(window, show="*")
 login_button = tk.Button(window, text="Login")
 
+
 # Placing widgets on the screen
-login_label.grid(row=0, column=0, columnspan=2, sticky="news", pady=40)
+login_label.grid(row=0, column=1, columnspan=2, sticky="news", pady=10)
 username_label.grid(row=1, column=0)
-username_entry.grid(row=1, column=1, pady=20)
+username_entry.grid(row=1, column=1, pady=10)
 password_label.grid(row=2, column=0)
-password_entry.grid(row=2, column=1, pady=20)
-login_button.grid(row=3, column=0, columnspan=2, pady=30)
+password_entry.grid(row=2, column=1, pady=10)
+login_button.grid(row=3, column=1, columnspan=2, pady=10)
 
 window.mainloop()
