09b64e699d04536bfb6f77a3efb87b092c36e47d
Kailin <68765813+kailinchu@users.noreply.github.com>
2022-10-19 18:12:35 -0400

    Add class structure and error messages for registration

--- dcm/registration.py
+++ dcm/registration.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+from tkinter import *
 from tkinter import messagebox
 import sqlite3
 import pathlib
@@ -7,112 +7,111 @@ from PIL import ImageTk, Image
 
 
 createDB()
-def checkEntries():
-    print("test")
-    error_msg = ""
-    register_error = False
-    if (username_entry_reg.get() == ''):
-        error_msg = "Username cannot be empty"
-        register_error = True
-    if (password_entry_reg.get() != password2_entry_reg.get()):
-        error_msg = "test"
-        register_error = True
+background = 'white'
 
-    if (register_error == False):        
-        try:
-            connection = sqlite3.connect('userdata.db')
-            cursor = connection.cursor()
-            cursor.execute("INSERT INTO accounts VALUES (:username, :password)", {
-                            'username': username_entry_reg.get(),
-                            'password': password_entry_reg.get()
-            })
-            connection.commit()
-            tk.messagebox.showinfo('confirmation', 'Record Saved')
+class RegistrationPage:
+    def __init__(self, window, max_accounts):
+        self.window = window
+        self.window.geometry('450x500')
+        self.width = 400
+        self.height = 400
+        self.window.minsize(self.width+30, self.height+30)
+        self.max_accounts = max_accounts
 
-        except Exception as ep:
-            messagebox.showerror('', ep) 
-    else:
-        messagebox.showerror('Error', error_msg)
+        # ========= Registration Frame =========
+        self.frame = Frame(
+            self.window,
+            bg = background,
+            width = self.width,
+            height= self.height,
+        )
+        self.frame.place(anchor="c", relx=0.5, rely=0.5)
+        self.frame.grid_propagate(False)
 
-window = tk.Tk()
-window.title("Pacemaker | Registration")
-window.iconbitmap(str(pathlib.Path(__file__).parent.resolve())+ "\logo.ico")
-#window.geometry('340x440')
-f = ('Trebuchet', 14)
+        # set weight of grid
+        self.frame.columnconfigure(0,weight=1)
+        self.frame.columnconfigure(1,weight=3)
 
-background = '#ebebeb'
+        # ========= Registration Text =========
+        self.header_text = "Register"
+        self.header = Label(self.frame, text = self.header_text, font=('Arial', 24, 'bold'), bg = background, fg='black')
+        self.header.grid(row=0, column=0, columnspan=2, pady=(50,10))
 
-#interface logo
-img = ImageTk.PhotoImage(Image.open("dcm/Complogo.png"))
+        # ========= Login Logo =========
+        self.logo_image = ImageTk.PhotoImage(Image.open("images/Complogo.png"))
+        self.logo = Label(self.frame, image = self.logo_image, bg=background)
+        self.logo.image = self.logo_image
+        self.logo.grid(row=1,column=0,columnspan=2,pady=10)
 
-register_fr = tk.Frame(
-    window,
-    bd = 2,
-    bg = background,
-    relief = tk.SOLID,
-    padx = 10,
-    pady = 10
-)
+        # ========= Login Entry =========
+        self.username_label = Label(self.frame, text="Username", bg=background)
+        self.username_entry = Entry(self.frame, bg=background)
+        self.password_label = Label(self.frame, text="Password", bg=background)
+        self.password_entry = Entry(self.frame, show="*", bg=background)
+        self.password2_label = Label(self.frame, text="Re-enter password", bg=background)
+        self.password2_entry = Entry(self.frame, show="*", bg=background)
+        self.register_button = Button(self.frame, text="Register", bg=background,command=lambda: self.checkEntries())
 
-register_fr.pack()
+        # formatting entries
+        self.username_label.grid(row=2, column=0, sticky='w',pady=5, padx=(25,0))
+        self.username_entry.grid(row=2, column=1)
+        self.password_label.grid(row=3, column=0, pady=5, sticky='w', padx=(25,0))
+        self.password_entry.grid(row=3, column=1)
+        self.password2_label.grid(row=4, column=0, pady=5, sticky='w', padx=(25,0))
+        self.password2_entry.grid(row=4, column=1)
+        self.register_button.grid(row=5, column=0, columnspan=2, pady=10)
 
-tk.Label(
-    register_fr,
-    image = img,
-    font = f
-).grid(row = 0, column = 1, sticky = tk.W, pady=10)
+    def checkEntries(self):
+        error_msg = ""
+        register_error = False
+        if (self.username_entry.get() == ''):
+            error_msg = "Username cannot be empty"
+            register_error = True
+        if (not(register_error) and self.password_entry.get() == ''):
+            error_msg = "Password cannot be empty"
+            register_error = True
+        if (not(register_error) and self.password_entry.get() != self.password2_entry.get()):
+            error_msg = "Passwords do not match"
+            register_error = True
 
-tk.Label(
-    register_fr,
-    text = "Username",
-    bg = background,
-    font = f
-).grid(row = 1, column = 0, sticky = tk.W, pady = 10)
+        if (register_error == False):        
+            try:
+                connection = sqlite3.connect('userdata.db')
+                cursor = connection.cursor()
+                
+                # check number of users
+                cursor.execute("SELECT COUNT(username) FROM accounts")
+                num_accounts = cursor.fetchone()[0]
 
-tk.Label(
-    register_fr,
-    text = "Password",
-    bg = background,
-    font = f
-).grid(row = 2, column = 0, sticky = tk.W, pady = 10)
+                if (num_accounts == None or num_accounts >= self.max_accounts):
+                    raise Exception("There are already 10 accounts. You have reached the maximum.")
+                
+                # ensure username does not yet exist
+                cursor.execute("SELECT COUNT (username) FROM accounts WHERE username = (:username)", {
+                                'username': self.username_entry.get()
+                })
+                new_user = cursor.fetchone()[0] == 0
+                
+                if (not(new_user)):
+                    raise Exception("This username is already in use")
 
-tk.Label(
-    register_fr,
-    text = "Re-enter password",
-    bg = background,
-    font = f
-).grid(row = 3, column = 0, sticky = tk.W, pady = 10)
+                cursor.execute("INSERT INTO accounts VALUES (:username, :password)", {
+                                'username': self.username_entry.get(),
+                                'password': self.password_entry.get()
+                })
+                connection.commit()
+                messagebox.showinfo('confirmation', 'User Saved')
 
-username_entry_reg = tk.Entry(
-    register_fr,
-    font = f
-)
+            except Exception as ep:
+                messagebox.showerror('', ep) 
+        else:
+            messagebox.showerror('Error', error_msg)
 
-password_entry_reg = tk.Entry(
-    register_fr,
-    font = f,
-    show='*'
-)
+def launchRegistration():
+    window = Tk()
+    RegistrationPage(window,10)
+    window.iconbitmap(str(pathlib.Path(__file__).parent.resolve())+ "\logo.ico")
+    window.mainloop()
 
-password2_entry_reg = tk.Entry(
-    register_fr,
-    font = f,
-    show='*'
-)
-
-
-register_button = tk.Button(
-    register_fr, 
-    width=15, 
-    text='Register', 
-    font=f, 
-    relief=tk.SOLID,
-    command=lambda: checkEntries()
-)
-
-username_entry_reg.grid(row = 1, column = 1,pady=10, padx=20)
-password_entry_reg.grid(row = 2, column = 1,pady=10, padx=20)
-password2_entry_reg.grid(row = 3, column = 1,pady=10, padx=20)
-register_button.grid(row = 4, column = 0, columnspan=2, pady=10)
-window.mainloop()
-
+if __name__ == '__main__':
+    launchRegistration()
