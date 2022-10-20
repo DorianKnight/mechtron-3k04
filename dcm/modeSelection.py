3ffced13a5a2eaa4e988b48e6ef3d01654283c28
Norange01 <67119887+Norange01@users.noreply.github.com>
2022-10-20 16:13:04 -0400

    Improves modeSelection window formatting and style

--- dcm/modeSelection.py
+++ dcm/modeSelection.py
@@ -7,9 +7,9 @@ background = 'white'
 class ModeSelect: 
     def __init__(self, window): 
         self.window = window
-        self.window.geometry('450x500')
-        self.width = 400
-        self.height = 450
+        self.window.geometry('350x280')
+        self.width = 300
+        self.height = 220
         self.window.minsize(self.width+30, self.height+30)
         self.window.iconbitmap("images\logo.ico")
         self.window.title("Pacemaker | Mode Selection")
@@ -34,26 +34,26 @@ class ModeSelect:
         self.msFrame.grid_propagate(False)
         
         #Definition of elements on the page
-        self.chooseLabel = Label(self.msFrame, text = "Please choose a pacing mode")
-        self.aooRadio = Radiobutton(self.msFrame, text = "AOO", variable = self.mode, value = "AOO")
-        self.vooRadio = Radiobutton(self.msFrame, text = "VOO", variable = self.mode, value = "VOO")
-        self.aaiRadio = Radiobutton(self.msFrame, text = "AAI", variable = self.mode, value = "AAI")
-        self.vviRadio = Radiobutton(self.msFrame, text = "VVI", variable = self.mode, value = "VVI")
-        self.nextButton = Button(self.msFrame, text = "Next", command = openMode)
+        self.chooseLabel = Label(self.msFrame, text = "Please choose a pacing mode", bg=background,padx=10,pady=20)
+        self.aooRadio = Radiobutton(self.msFrame, text = "AOO", variable = self.mode, value = "AOO", bg=background,padx=10)
+        self.vooRadio = Radiobutton(self.msFrame, text = "VOO", variable = self.mode, value = "VOO", bg=background,padx=10)
+        self.aaiRadio = Radiobutton(self.msFrame, text = "AAI", variable = self.mode, value = "AAI", bg=background,padx=10)
+        self.vviRadio = Radiobutton(self.msFrame, text = "VVI", variable = self.mode, value = "VVI", bg=background,padx=10)
+        self.nextButton = Button(self.msFrame, text = "Next", command = openMode,bg=background, width=8)
         
         #Formatting placement of elements on the page
-        self.chooseLabel.grid(row = 1, column = 1, columnspan = 8)
-        self.aooRadio.grid(row = 2, column = 1, sticky = W)
-        self.vooRadio.grid(row = 3, column = 1, sticky = W)
-        self.aaiRadio.grid(row = 4, column = 1, sticky = W)
-        self.vviRadio.grid(row = 5, column = 1, sticky = W)
-        self.nextButton.grid(row = 6, column = 2, sticky = W)
+        self.chooseLabel.grid(row = 0, column = 0, columnspan = 2)
+        self.aooRadio.grid(row = 1, column = 0, sticky = W)
+        self.vooRadio.grid(row = 2, column = 0, sticky = W)
+        self.aaiRadio.grid(row = 3, column = 0, sticky = W)
+        self.vviRadio.grid(row = 4, column = 0, sticky = W)
+        self.nextButton.grid(row = 5, column = 1, columnspan=2, sticky = W)
                 
         
     
 def launchModeSelect(): 
     window = Tk()
-    ModeSelect(window)
+    ModeSelect(window).aooRadio.select()
     window.mainloop()
     
 if __name__ == '__main__':
