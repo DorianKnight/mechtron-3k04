b6b4b03e846b2de99c51d1b089126da81da23085
Kailin <68765813+kailinchu@users.noreply.github.com>
2022-10-22 17:35:29 -0400

    Makes table with all parameters

--- dcm/data.py
+++ dcm/data.py
@@ -7,7 +7,21 @@ def createDB():
     # create database table
     cursor.execute('''CREATE TABLE IF NOT EXISTS accounts (
                         username text,
-                        password text
+                        password text,
+                        lrl INTEGER,
+                        url INTEGER,
+                        apw REAL,
+                        vpw REAL,
+                        aamp REAL,
+                        vamp REAL,
+                        asens REAL,
+                        vsens REAL,
+                        arp INTEGER,
+                        vrp INTEGER,
+                        pvarp INTEGER,
+                        hyst boolean,
+                        hrl INTEGER,
+                        rs INTEGER
                 )
                 ''')
     connection.commit()
