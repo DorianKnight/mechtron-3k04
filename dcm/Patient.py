3a0c244f812ff756b114ddc835aedf6cebaef967
Kabir Singh Gupta <guptak15@mcmaster.ca>
2022-10-23 12:18:37 -0400

    Added nominal values

--- dcm/Patient.py
+++ dcm/Patient.py
@@ -25,18 +25,18 @@ class Patient:
     def __init__(self):
         self.username="NONE"
 
-        self.lrl = 0
-        self.url = 0
+        self.lrl = 60
+        self.url = 120
         self.pacingMode = "NONE"
-        self.apw = 0.0
-        self.vpw = 0.0
-        self.aamp = 0.0
-        self.vamp = 0.0
-        self.asens = 0.0
-        self.vsens = 0.0
-        self.arp = 0
-        self.vrp = 0
-        self.pvarp = 0
+        self.apw = 0.4
+        self.vpw = 0.4
+        self.aamp = 3.5
+        self.vamp = 3.5
+        self.asens = 0.75
+        self.vsens = 2.5
+        self.arp = 250
+        self.vrp = 320
+        self.pvarp = 250
         self.hrl = 0
         self.rs = 0
 
