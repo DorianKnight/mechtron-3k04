6933cc43b54461a2cfeba59400b826dd03e6662d
Dorian Knight <113370795+KnightDorian@users.noreply.github.com>
2022-11-17 13:27:56 -0500

    Adds the ability to send parameters and read parameters from FRDM board
    This commit adds the python file needed to both send and receive parameters from the pacemaker.
    This commit also adds in the corrective factor to ensure that the rate adaptive lrl paces the heart as expected.
    
    The rate adaptive modes don't work as desired yet, you can have your heart pace at the LRL however the heart rate doesn't change when you shake the board however when I run monitor and tune, the board works as expected.
    
    For some reason the corrective timing factor does not work for VVI.

---
+++ simulink_group44/SerialCommunications.py
@@ -0,0 +1,66 @@
+#Author: Group 44
+#Date: November 17 2022
+#Purpose: To link the DCM and simulink model together through serial communications
+
+import serial
+
+def SendData(ser):
+    if (ser.is_open):
+        #Set the parameters to bytes and combine into one data structure
+        SYNC = (16).to_bytes(1,byteorder='little')
+        SetOrEcho = (55).to_bytes(1,byteorder='little')
+        LRL = (60).to_bytes(1,byteorder='little')
+        MSR = (180).to_bytes(1,byteorder='little')
+        aPulseAmp = (4000).to_bytes(2,byteorder='little')
+        aPulseWidth = (1).to_bytes(1,byteorder='little')
+        aSensitivity = (2700).to_bytes(2,byteorder='little')
+        ARP = (100).to_bytes(2,byteorder='little')
+        vPulseAmp = (3000).to_bytes(2,byteorder='little')
+        vPulseWidth = (1).to_bytes(1,byteorder='little')
+        vSensitivity = (2500).to_bytes(2,byteorder='little')
+        VRP = (320).to_bytes(2,byteorder='little')
+        AVDelay = (150).to_bytes(2,byteorder='little')
+        PVARP = (250).to_bytes(2,byteorder='little')
+        ATH = (6).to_bytes(1,byteorder='little')
+        RF = (8).to_bytes(1,byteorder='little')
+        reactionT = (30).to_bytes(1,byteorder='little')
+        recoveryT = (5).to_bytes(1,byteorder='little')
+        mode = (1).to_bytes(1,byteorder='little')
+
+        data = SYNC + SetOrEcho + LRL + MSR + aPulseAmp + aPulseWidth + aSensitivity + ARP + vPulseAmp + vPulseWidth + vSensitivity + VRP + AVDelay + PVARP + ATH + RF + reactionT + recoveryT + mode
+
+        #Send data to FRDM board
+        ser.write(data)
+
+        #If echo, read for echoed params
+        if (SetOrEcho == (22).to_bytes(1,byteorder='little')):
+            boardVals = ser.read(25) # Reads all the params sent which excludes SYNC and SetOrEcho
+            print("LRL:",boardVals[0],
+            "\nMSR:",boardVals[1],
+            "\naPulseAmp:",int.from_bytes(boardVals[2:4],'little'),
+            "\naPulseWidth:",boardVals[4],
+            "\naSensitivity:",int.from_bytes(boardVals[5:7],'little'),
+            "\nARP:",int.from_bytes(boardVals[7:9],'little'),
+            "\nvPulseAmp:",int.from_bytes(boardVals[9:11],'little'),
+            "\nvPulseWidth:",boardVals[11],
+            "\nvSensitivity:",int.from_bytes(boardVals[12:14],'little'),
+            "\nVRP:",int.from_bytes(boardVals[14:16],'little'),
+            "\nAVDelay:",int.from_bytes(boardVals[16:18],'little'),
+            "\nPVARP:",int.from_bytes(boardVals[18:20],'little'),
+            "\nATH:",boardVals[20],
+            "\nRF:",boardVals[21],
+            "\nreactionT:",boardVals[22],
+            "\nrecoveryT:",boardVals[23],
+            "\nmode:",boardVals[24])
+
+    
+    else:
+        #Display error message saying that the serial port is not open
+        pass
+
+print("\n\n\n")
+ser = serial.Serial()
+ser.baudrate = 115200
+ser.port = 'COM7'
+ser.open()
+SendData(ser)
