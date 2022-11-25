9a3cba8d9dfcdc15fd71f04b68e7061ba625b376
Norange01 <67119887+Norange01@users.noreply.github.com>
2022-11-25 11:47:42 -0500

    Places connection status variables outside the functions for it to be accessed from other files and classes

--- DCM_group44/connectionDisplay.py
+++ DCM_group44/connectionDisplay.py
@@ -1,9 +1,11 @@
 from tkinter import *
 
+connectionChecker=False
+newDeviceChecker=False
+
 # ========= Connection =========
 # display whether the DCM is connected to the pacemaker
 def displayConnection(window):
-    connectionChecker=False
     if(connectionChecker==False):
         connectionBanner=Label(window,text="Not connected - ", fg= 'red', font=("Helvetica",12), padx=10)
         connectionBanner.grid(row=0,column=0, sticky=W)
@@ -14,7 +16,6 @@ def displayConnection(window):
 # ========= New Device =========
 # display whether the DCM is connected to a new pacemaker
 def displayNewDevice(window):    
-    newDeviceChecker=False
     if(newDeviceChecker==False):
         deviceBanner = Label(window,text="No new device",fg='black', font=("Helvetica", 12), padx=10)
         deviceBanner.grid(row=0,column=2, sticky=E)
