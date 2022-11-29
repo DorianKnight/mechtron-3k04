9d08e07d6e0530be96a42c093666d99892b32d2a
Dorian Knight <113370795+KnightDorian@users.noreply.github.com>
2022-11-28 21:34:32 -0500

    Fixes bug in egrams when you close the graphs
    This commit ensures that you can't access the pacemaker functions when you're viewing egrams and fixes a bug where the DCM gets stuck when viewing the dual chamber egrams. This commit also adds labels and titles on the egrams graphs.

--- DCM_group44/EgramsWindow.py
+++ DCM_group44/EgramsWindow.py
@@ -20,6 +20,7 @@ class EgramsDisplay:
         self.patient=patient
         self.token = token #This allows me to know where you've come from so we can send you back when you want to leave
         self.EgramsPlotObject = EgramsPlotting(100,self.patient,Serobj) #Initializes the serial communication object
+        self.Serobj = Serobj
 
         self.egrams_frame = Frame(
             self.window,
@@ -72,15 +73,21 @@ class EgramsDisplay:
             
             if (self.atrialDisplay.get() == 1 and self.ventricularDisplay.get() == 0):
                 #Only plot the atria and not the ventricular egram
+                self.window.withdraw()
                 self.EgramsPlotObject.DisplayEgramsAtria()
+                self.window.deiconify()
 
             elif (self.atrialDisplay.get() == 0 and self.ventricularDisplay.get() == 1):
                 #Only plot the ventricular and not the atrial egram
+                self.window.withdraw()
                 self.EgramsPlotObject.DisplayEgramsVentricle()
+                self.window.deiconify()
 
             else:
                 #Plot both the ventricular and atrial egrams data
+                self.window.withdraw()
                 self.EgramsPlotObject.DisplayEgramsDualChamber()
+                self.window.deiconify()
 
 
     def goBack(self):
@@ -91,34 +98,34 @@ class EgramsDisplay:
             if(self.token == 'pacingModes'):
                 #Find out which pacing mode you came from
                 if(self.patient.pacingMode == 'AOO'):
-                    pacingModes.launchAOO(self.window,self.patient, self.EgramsPlotObject)
+                    pacingModes.launchAOO(self.window,self.patient, self.Serobj)
                 elif(self.patient.pacingMode == 'AOOR'):
-                    pacingModes.launchAOOR(self.window,self.patient, self.EgramsPlotObject)
+                    pacingModes.launchAOOR(self.window,self.patient, self.Serobj)
                 elif(self.patient.pacingMode == 'AAI'):
-                    pacingModes.launchAAI(self.window,self.patient, self.EgramsPlotObject)
+                    pacingModes.launchAAI(self.window,self.patient, self.Serobj)
                 elif(self.patient.pacingMode == 'AAIR'):
-                    pacingModes.launchAAIR(self.window,self.patient, self.EgramsPlotObject)
+                    pacingModes.launchAAIR(self.window,self.patient, self.Serobj)
                 elif(self.patient.pacingMode == 'VOO'):
-                    pacingModes.launchVOO(self.window,self.patient, self.EgramsPlotObject)
+                    pacingModes.launchVOO(self.window,self.patient, self.Serobj)
                 elif(self.patient.pacingMode == 'VOOR'):
-                    pacingModes.launchVOOR(self.window,self.patient, self.EgramsPlotObject)
+                    pacingModes.launchVOOR(self.window,self.patient, self.Serobj)
                 elif(self.patient.pacingMode == 'VVI'):
-                    pacingModes.launchVVI(self.window,self.patient, self.EgramsPlotObject)
+                    pacingModes.launchVVI(self.window,self.patient, self.Serobj)
                 elif(self.patient.pacingMode == 'VVIR'):
-                    pacingModes.launchVVIR(self.window,self.patient, self.EgramsPlotObject)
+                    pacingModes.launchVVIR(self.window,self.patient, self.Serobj)
                 elif(self.patient.pacingMode == 'DDD'):
-                    pacingModes.launchDDD(self.window,self.patient, self.EgramsPlotObject)
+                    pacingModes.launchDDD(self.window,self.patient, self.Serobj)
                 elif(self.patient.pacingMode == 'DDDR'):
-                    pacingModes.launchDDDR(self.window,self.patient, self.EgramsPlotObject)
+                    pacingModes.launchDDDR(self.window,self.patient, self.Serobj)
                 else:
                     #Send them back to the mode select page
                     print("You don't have a pacing mode enabled")
                     #Flag an error for communication 
-                    modeSelection.launchModeSelect(self.patient.username, self.window, self.EgramsPlotObject)
+                    modeSelection.launchModeSelect(self.patient.username, self.window, self.Serobj)
 
             #If your previous window was the select mode window or something that I can't forsee at the moment
             else:
-                modeSelection.launchModeSelect(self.patient.username, self.window, self.EgramsPlotObject)
+                modeSelection.launchModeSelect(self.patient.username, self.window, self.Serobj)
         
 
 '''
